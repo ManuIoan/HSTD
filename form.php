7f6e35304ab064abf4e735d1f54d92160b339617
Manu Ioan <ioanmanu@yahoo.com>
2021-10-23 09:16:55 +0300

    form 0.0.1

---
+++ form.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cs1">
+      <form method="post" name="myemailform" action="form-to-email.php" class="f100">
+
+
+
+        <label for="">Name&#128076;</label>
+        <input type="text" name="name" class="big" required>
+
+        <label for="">Email&#128237;</label>
+        <input type="text" name="email" class="big" required>
+
+        <label for="">Message&#128220; </label>
+        <textarea name="message" class="bigar" cols="30" rows="10" required></textarea>
+         
+        <input type="submit" class="smolar" name="submit">
+
+
+
+      </form>
+    </div>
+
+</body>
+</html>
